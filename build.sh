a0cce090e26e80ee9bf0f73a5d5749b0c31d537a
Adonis Martin <adonisdesuvikdo@gmail.com>
2022-10-01 17:13:28 -0700

    try 8

--- build.sh
+++ build.sh
@@ -5,9 +5,8 @@
 # and saves the output into "output_temp"
 
 OUTPUT_DIR="output_temp"
-TARGET_DIR="storybook-static"
 
 mkdir "$OUTPUT_DIR"
 yarn build-storybook
 
-cp -a TARGET_DIR OUTPUT_DIR
+cp -a "storybook-static" OUTPUT_DIR
